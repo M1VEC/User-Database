43faba14ae5d5ddbc77de1fed31fee4ae0874be1
M1VEC <pbilton85@gmail.com>
2020-06-28 17:05:42 +0100

    added new user loop

--- src/com/pbilton/userDatabase/Main.java
+++ src/com/pbilton/userDatabase/Main.java
@@ -2,19 +2,25 @@ package com.pbilton.userDatabase;
 
 import java.util.ArrayList;
 import java.util.Scanner;
-
+//Want to add a user menu to add new user, search user and delete user
+//add a test file that adds 10 users
 public class Main {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
         ArrayList<user> users = new ArrayList<>();
-
-        users.add(user.create(getInput("User Name"), getInput("Email"), getInput("Company")));
-        users.add(user.create(getInput("User Name"), getInput("Email"), getInput("Company")));
+        String additionalUser = " ";
+            while (!additionalUser.equals("n")) {
+                addUser(users);
+                System.out.println("Add another user: y or n");
+                additionalUser = scanner.next().toLowerCase();
+            }
 
         System.out.println(users.get(0).print());
-        System.out.println(users.get(1).print());
+    }
+    private static void addUser(ArrayList<user> users){
+        users.add(user.create(getInput("User Name"), getInput("Email"), getInput("Company")));
     }
 
     private static String getInput(String prompt) {
